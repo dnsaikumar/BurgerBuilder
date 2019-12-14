bb0c45a539db75903ef32e2801325a140949778a
D.N.Sai Kumar <saikumardasari32@outlook.com>
2019-12-14 17:48:50 +0000

    Commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Auxil from '../../hoc/Aux';
+import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        })
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        });
+    }
+    render() {
+        return (
+            < Auxil >
+                {/* <div>ToolBar, SideBr, Backdrop</div> */}
+                < Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxil >
+        );
+    }
+}
+
+export default Layout;
